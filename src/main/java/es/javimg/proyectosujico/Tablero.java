184ffc43ed0ead638ff3b70e22ff906d2e550c83
jmangam <jmangam010@g.educaand.es>
2022-03-17 10:33:23 +0100

    JMG - Rellenando tablero

--- src/main/java/es/javimg/proyectosujico/Tablero.java
+++ src/main/java/es/javimg/proyectosujico/Tablero.java
@@ -1,5 +1,6 @@
 package es.javimg.proyectosujico;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 
@@ -10,6 +11,8 @@ public class Tablero{
 Cuadrado cuadrado = new Cuadrado();
 Circulo circulo = new Circulo();
 
+
+
     int x=100;
     int y=100;
     int xc=500;
@@ -17,24 +20,28 @@ Circulo circulo = new Circulo();
     
     public void montarTablero(Pane paneRoot){
                    
-        for(int x=0; x<300; x++) {
-            x= x+100;
-            System.out.println("x:" + x);
-            for(int y=0; y<300; y++) {
+        for(int x=0; x<300; x+=100) {
+            for(int y=0; y<300; y+=100) {
                 cuadrado.crearCuadrado(paneRoot, x, y);
-                y= y+100;
-                System.out.println("y:" + y);
             }
         }  
         
-        for(int xc=100; xc<300; x++) {
-            xc= xc+100;
-            for(int yc=100; yc<300; y++) {
+        for(int xc=100; xc<300; xc+=100) {
+            for(int yc=100; yc<300; yc+=100) {
                 circulo.crearCirculo(paneRoot, xc, yc);
-                yc= yc+100;
             }
         } 
         
     }
     
+    public void rellenarTablero(Pane paneRoot) {
+        
+            for(int x=0;x<3;x++) {
+                for (int y=0;y<3;y++) {
+                    Label label = new Label(numeros[x][y]);
+                }
+            }
+
+        
+    }
 }
